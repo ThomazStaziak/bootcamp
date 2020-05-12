ab8e2ba4fbbdb03b3de63e7a1d0feb306edf1200
ThomazStaziak <thostaziak@gmail.com>
2020-05-12 16:48:41 -0300

    chore: connected the smtp protocol with nodemailer

---
+++ src/config/mail.js
@@ -0,0 +1,12 @@
+export default {
+  host: 'smtp.mailtrap.io',
+  port: 2525,
+  secure: false,
+  auth: {
+    user: 'e8fda88637c4b8',
+    pass: '2862cc24861051',
+  },
+  default: {
+    from: 'Equipe GoBarber <noreply@gobarber.com>',
+  },
+};
